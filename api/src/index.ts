40d8cb48b84d7335385796dff53870cbcc89d764
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-06-06 21:11:16 +0530

    feat(api): query complexity

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,11 @@ import express from "express";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import cors from "cors";
+import {
+  fieldExtensionsEstimator,
+  getComplexity,
+  simpleEstimator,
+} from "graphql-query-complexity";
 
 import CreateSchema from "./util/CreateSchema";
 
@@ -15,6 +20,34 @@ import CreateSchema from "./util/CreateSchema";
   const gqlServer = new ApolloServer({
     schema,
     context: ({ req, res }) => ({ req, res }),
+    plugins: [
+      {
+        requestDidStart: () => ({
+          didResolveOperation({ request, document }) {
+            const complexity = getComplexity({
+              schema,
+              operationName: request.operationName,
+              query: document,
+              variables: request.variables,
+              estimators: [
+                fieldExtensionsEstimator(),
+                simpleEstimator({ defaultComplexity: 1 }),
+              ],
+            });
+
+            const maximumQueryComplexity = 15;
+
+            if (complexity > maximumQueryComplexity) {
+              console.log(complexity);
+
+              throw new Error(
+                `Maximum query complexity of ${maximumQueryComplexity} has been reached.`
+              );
+            }
+          },
+        }),
+      },
+    ],
   });
 
   // Create Express Server
