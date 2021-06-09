c64fcb1b9d5ae84c996436bf63e66d42abd6e90c
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-06-09 12:56:37 +0530

    chore(api-new): move resolvers to api-new dir (untested)

---
+++ api-new/src/resolvers/Project/CreateProject/CreateProject.resolver.ts
@@ -0,0 +1,28 @@
+import Project from "../../../db/entity/Project.entity";
+import { Mutation, Resolver } from "type-graphql";
+import User from "../../../db/entity/User.entity";
+import faker from "faker";
+
+@Resolver()
+export default class CreateProjectResolver {
+  @Mutation(() => Project)
+  async createProject() {
+    let user = (await User.find({ relations: ["profile"] }))[0];
+
+    let project = new Project();
+
+    project.title = faker.lorem.words();
+    project.githubId = faker.datatype.number(9999999999);
+    project.owner = user!;
+    project.collaborators = [user!];
+    project.likers = [user!];
+
+    project = await project.save();
+
+    user.likedProjects = [project];
+    user = await user.save();
+
+    await project.reload();
+    return project;
+  }
+}
