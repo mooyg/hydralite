6d1f7b6a025dc78698a944dc664b9887850586a5
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-06-09 12:03:58 +0530

    chore(api-new): make resolver import use path to resolvers

--- api-new/src/util/CreateSchema.ts
+++ api-new/src/util/CreateSchema.ts
@@ -1,9 +1,8 @@
 import { GraphQLSchema } from "graphql";
-import MainResolver from "../resolvers/Main/Main.resolver";
 import { buildSchema } from "type-graphql";
 
 export default async function CreateSchema(): Promise<GraphQLSchema> {
   return buildSchema({
-    resolvers: [MainResolver],
+    resolvers: [__dirname + "\\..\\resolvers\\**\\*.resolver.js"],
   });
 }
