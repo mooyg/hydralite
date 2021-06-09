8ef7f4e6a459c43cbc72f2a750bcf1fe86e59181
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-06-09 12:55:31 +0530

    chore(api-new): migrate typeorm entities to mikroorm entities (untested)

--- api-new/src/db/entity/UserProfile.entity.ts
+++ api-new/src/db/entity/UserProfile.entity.ts
@@ -1,5 +1,6 @@
 import { Field, ID, ObjectType } from "type-graphql";
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity, OneToOne, PrimaryKey, Property } from "@mikro-orm/core";
+import User from "./User.entity";
 
 @ObjectType()
 @Entity({ tableName: "profiles" })
@@ -7,6 +8,10 @@ export default class UserProfile {
   @Field(() => ID)
   @PrimaryKey()
   id!: number;
+
+  @Field(() => User)
+  @OneToOne(() => User, (user) => user.profile)
+  user: User;
 
   @Field()
   @Property()
