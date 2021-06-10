52e009c056f220472a751c931cdbaeda4467da46
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-06-10 18:15:05 +0530

    feat(api): implement session tokens

--- api/src/modules/resolvers/User/UserSignOn.resolver.ts
+++ api/src/modules/resolvers/User/UserSignOn.resolver.ts
@@ -1,21 +1,27 @@
 // dependencies
-import { Arg, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
 import OauthConnection from "~/db/entity/OauthConnection.entity";
 import User from "~/db/entity/User.entity";
 import UserRepo from "~/db/repos/User.repo";
 import { UserSignOnInput } from "~/modules/input/User/UserSignOn.input";
+import ContextType from "~/types/Context.type";
 import executeOrFail from "~/util/executeOrFail";
 import { fetchDiscordUser } from "~/util/fetchOauthUser";
 
 @Resolver()
 export default class UserSignOnResolver {
   @Mutation(() => User)
-  async userSignOn(@Arg("input") input: UserSignOnInput): Promise<User | null> {
+  async userSignOn(
+    @Arg("input") input: UserSignOnInput,
+    @Ctx() { req }: ContextType
+  ): Promise<User | null> {
     switch (input.provider) {
       case "discord":
         // fetch discord user by id
         return executeOrFail(async () => {
           const discordUser = await fetchDiscordUser(input.accessToken);
+
+          let user;
 
           // try to query oauth connections to see if a user exists
           const existingUser = await OauthConnection.findOne(
@@ -32,10 +38,18 @@ export default class UserSignOnResolver {
             }
           );
 
-          // user does not exist
-          return !existingUser
-            ? await UserRepo.createDiscordUser(discordUser)
-            : existingUser.owner;
+          // user doesnt exist
+          if (!existingUser) {
+            user = await UserRepo.createDiscordUser(discordUser);
+          } else {
+            user = existingUser;
+          }
+
+          // set session
+          (req.session as any).userId = user.id;
+
+          // return user
+          return user;
         }, "Error creating user.");
       default:
         throw new Error("Invalid Oauth Provider.");
