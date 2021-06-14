c7c2c71ff6e494bf18a3ef39f284ae8f0b9024dc
Shadow7258 <pranav.rdoshi@gmail.com>
2021-06-14 17:18:40 +0530

    feat: dashboard screen

---
+++ web/pages/dashboard.tsx
@@ -0,0 +1,54 @@
+import styles from '../styles/Dashboard.module.css'
+
+// icons
+import { MdNotifications } from 'react-icons/md'
+
+const Dashboard = () => {
+  return (
+    <div className={styles.screen}>
+      <Navbar />
+      <Sidebar />
+    </div>
+  )
+}
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.logoDiv}>
+        <img src='/logo.png' alt='Logo' className={styles.logo} />
+      </div>
+      <div className={styles.nav_right}>
+        <img
+          src='/icons/notifications.svg'
+          className={styles.notif}
+          alt='Notifications'
+        />
+        <img src='/sample_user.svg' alt='Logo' className={styles.avatar} />
+      </div>
+    </div>
+  )
+}
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.icons}>
+        <img src='/icons/home.svg' alt='Home' className={styles.icon} />
+        <img src='/icons/projects.svg' alt='Projects' className={styles.icon} />
+        <img src='/icons/messages.svg' alt='Messages' className={styles.icon} />
+        <img
+          src='/icons/following.svg'
+          alt='Following'
+          className={styles.icon}
+        />
+        <img src='/icons/profile.svg' alt='Profile' className={styles.icon} />
+      </div>
+      <div className={styles.createButton}>
+        <img src='/icons/plus.svg' alt='Plus' className={styles.icon} />
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
