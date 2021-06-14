3cec94cdd57b18b6312482667082299b7e908ef9
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-06-14 13:32:37 +0530

    add(web) - heading text

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "~/components/Landing/Navbar";
+import styles from "~/styles/index.module.css";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <div className={styles.main}>
+        <h1 className={styles.main__text}>
+          A Place for Developers <br />
+          to <span className={styles.main__text_Gradient}>Deliver</span>{" "}
+          Software
+        </h1>
+      </div>
     </div>
   );
 };
