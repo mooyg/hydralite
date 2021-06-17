cc9c04807cc5c1939ade3514e1f94d19e5040cd2
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-06-17 21:27:00 +0530

    chore(global) - modify folders

---
+++ landing/components/Layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '~/components/Navbar'
+import Footer from './Footer'
+
+const Layout = ({
+    children,
+    navType,
+    waitlistCardRef,
+}: {
+    children: any
+    navType: 'home' | 'dash'
+    waitlistCardRef: React.MutableRefObject<any>
+}) => {
+    return (
+        <div>
+            {navType === 'home' && <Navbar waitlistCardRef={waitlistCardRef} />}
+            {children}
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
