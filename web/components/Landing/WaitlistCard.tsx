518e46e279104bd30b50f45e624ab4f940a781d5
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-06-17 12:01:37 +0530

    add(web) - waitlist card

--- web/components/Landing/WaitlistCard.tsx
+++ web/components/Landing/WaitlistCard.tsx
@@ -7,8 +7,13 @@ const WaitlistCard = () => {
             <h3>{'What are you waiting for?'.toUpperCase()}</h3>
             <h1>Sign Up For The Waitlist!</h1>
             <div className={styles.waitlistCard__textbox}>
+                <img src="/icons/at.svg" alt="" />
                 <input type="email" placeholder={'email'.toUpperCase()} />
+                <button className={styles.waitlistCard__submit}>Submit</button>
             </div>
+            <span className={styles.waitlistCard__subText}>
+                We promise to never spam you.
+            </span>
         </div>
     )
 }
