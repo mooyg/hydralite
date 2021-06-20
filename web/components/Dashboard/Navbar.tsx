1ec647f6fec082f1b71a44aee9b16e752a2b2a9f
Shadow7258 <pranav.rdoshi@gmail.com>
2021-06-20 15:30:32 +0530

    feat(web) - dashboard navbar

--- web/components/Dashboard/Navbar.tsx
+++ web/components/Dashboard/Navbar.tsx
@@ -1,5 +1,50 @@
+import styles from "./Navbar.module.scss";
+
 const Navbar = () => {
-    
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.navbar_left}>
+                <img src="/logo.png" alt="Logo" className={styles.logo} />
+                <div className={styles.nav_group}>
+                    <h3>Dashboard</h3>
+                </div>
+                <div className={styles.nav_group}>
+                    <h3>Projects</h3>
+                    <img src="/icons/down_arrow.svg" alt="down" />
+                </div>
+                <div className={styles.nav_group}>
+                    <h3>Discover</h3>
+                    <img src="/icons/down_arrow.svg" alt="down" />
+                </div>
+                <div className={styles.nav_group}>
+                    <h3>Communicate</h3>
+                    <img src="/icons/down_arrow.svg" alt="down" />
+                </div>
+                <div className={styles.nav_group}>
+                    <h3>More</h3>
+                    <img src="/icons/down_arrow.svg" alt="down" />
+                </div>
+            </div>
+            <div className={styles.navbar_right}>
+                <img
+                    src="icons/search.svg"
+                    alt="search"
+                    className={styles.nav_icon}
+                />
+                <img
+                    src="icons/notifications.svg"
+                    alt="notifications"
+                    className={styles.nav_icon}
+                />
+                <img src="/avatar.svg" alt="avatar" className={styles.avatar} />
+                <img
+                    src="/icons/down_arrow.svg"
+                    alt="down"
+                    className={styles.down_arrow}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
--- web/pages/dashboard.tsx
+++ web/pages/dashboard.tsx
@@ -1,5 +1,12 @@
+import Navbar from "../components/Dashboard/Navbar";
+import styles from "../styles/Dashboard.module.scss";
+
 const Dashboard = () => {
-    return <div></div>;
+    return (
+        <div className={styles.dashboard}>
+            <Navbar />
+        </div>
+    );
 };
 
 export default Dashboard;
