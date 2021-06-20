a0eb3034bd528ecf9ee2fe27d4b64b576cb292c2
Shadow7258 <pranav.rdoshi@gmail.com>
2021-06-20 15:14:02 +0530

    fix(web) button scss import

--- web/components/Button/Button.tsx
+++ web/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FC } from "react";
-import styles from "./Button.module.css";
+import styles from "./Button.module.scss";
 import Icon from "~/components/Icon";
 import Link from "next/link";
 
