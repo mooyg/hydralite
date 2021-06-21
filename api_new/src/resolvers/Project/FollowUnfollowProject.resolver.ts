15c6cc3d17c1c5560398c3e8873f51e3e303760b
Aaryaman Maheshwari <aaryaman2007@outlook.com>
2021-06-21 16:36:22 +0530

    feat(api_new) - follow unfollow project mutations

---
+++ api_new/src/resolvers/Project/FollowUnfollowProject.resolver.ts
@@ -0,0 +1,74 @@
+import { User } from "@prisma/client";
+import { isAuthenticated } from "src/middleware/IsAuthenticated.middleware";
+import {
+    Arg,
+    Ctx,
+    Field,
+    InputType,
+    Mutation,
+    UseMiddleware,
+} from "type-graphql";
+import ContextType from "~/types/Context.type";
+import executeOrFail from "~/util/executeOrFail";
+
+@InputType()
+export class FollowUnfollowProjectInput {
+    @Field()
+    projectId: string;
+}
+
+export default class FollowUnfollowProjectResolver {
+    @UseMiddleware(isAuthenticated)
+    @Mutation(() => String)
+    async followProject(
+        @Arg("input") { projectId }: FollowUnfollowProjectInput,
+        @Ctx() { req, prisma }: ContextType
+    ): Promise<string> {
+        return executeOrFail(async () => {
+            // retrieve the currently logged in user
+            const user: User = (req as any).user;
+
+            await prisma.project.update({
+                where: {
+                    id: projectId,
+                },
+                data: {
+                    followers: {
+                        connect: {
+                            id: user.id,
+                        },
+                    },
+                },
+            });
+
+            return "Followed project.";
+        });
+    }
+
+    @UseMiddleware(isAuthenticated)
+    @Mutation(() => String)
+    async unfollowProject(
+        @Arg("input") { projectId }: FollowUnfollowProjectInput,
+        @Ctx() { req, prisma }: ContextType
+    ): Promise<string> {
+        return executeOrFail(async () => {
+            // retrieve the currently logged in user
+            const user: User = (req as any).user;
+
+            await prisma.project.update({
+                where: {
+                    id: projectId,
+                },
+                data: {
+                    followers: {
+                        disconnect: {
+                            id: user.id,
+                        },
+                    },
+                },
+            });
+
+            return "Unfollowed project.";
+        });
+    }
+}
