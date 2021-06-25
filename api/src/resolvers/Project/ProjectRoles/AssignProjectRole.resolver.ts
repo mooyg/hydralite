596e190271d2f16d27dd8beedc4db90658456aaf
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-06-25 20:10:55 +0530

    feat(api) - assignProjectRole mutation

---
+++ api/src/resolvers/Project/ProjectRoles/AssignProjectRole.resolver.ts
@@ -0,0 +1,45 @@
+import {
+    Arg,
+    Ctx,
+    Field,
+    InputType,
+    Mutation,
+    Resolver,
+    UseMiddleware,
+} from "type-graphql";
+import ContextType from "~/types/Context.type";
+import { isAuthenticated } from "~/middleware/isAuthenticated.middleware";
+import { ProjectMember } from "~/typegql-types/ProjectMember";
+
+@InputType()
+export class AssignProjectRoleInput {
+    @Field()
+    userId: string;
+
+    @Field()
+    roleId: string;
+}
+
+@Resolver()
+export default class AssignProjectRoleResolver {
+    @Mutation(() => ProjectMember)
+    @UseMiddleware(isAuthenticated)
+    async assignProjectRole(
+        @Arg("input") input: AssignProjectRoleInput,
+        @Ctx() { req, prisma }: ContextType
+    ): Promise<ProjectMember | null> {
+        // retrieve the currently logged in user
+        // const user: User = req.user as User;
+
+        // TASK: only allow users with the role management permission to assign roles
+
+        const updatedMember = await prisma.projectMember.update({
+            where: { id: input.userId },
+            data: { roles: { connect: { id: input.roleId } } },
+        });
+
+        // TASK: run algo to update project members common permissions as well
+
+        return updatedMember;
+    }
+}
